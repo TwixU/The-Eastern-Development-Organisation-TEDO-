7e8a54a52457571cbdcef5b917024a2e3fff090d
TwixU <kubratahincioglu879182@gmail.com>
2025-12-18 02:12:16 -0800

    Fix white screen issue: Downgrade router, remove ScrollRestoration, add ScrollToTop

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 
 const Layout = () => {
     return (
         <div className="layout">
+            <ScrollToTop />
             <Navbar />
             <main>
                 <Outlet />
             </main>
             <Footer />
-            <ScrollRestoration />
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+console.log('Main starting...');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
